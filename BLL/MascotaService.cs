15684e700e00410481708b4531b90f21bc0ee8b2
Raikadier <138398472+Raikadier@users.noreply.github.com>
2025-05-17 18:02:12 -0500

    Add files via upload

---
+++ BLL/MascotaService.cs
@@ -0,0 +1,100 @@
+﻿using DAL;
+using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class MascotaService:IService<Mascota>
+    {
+        private readonly MascotaRepository repoMascota;
+
+        public MascotaService()
+        {
+            repoMascota = new MascotaRepository(Archivos.ARC_MASCOTA);
+        }
+
+        public List<Mascota> Consultar()
+        {
+            return repoMascota.Consultar();
+        }
+        //public List<RazaDto> ConsultarDTO()
+        //{
+        //    return repoRaza.ConsultarDTO();
+        //}
+        public string Guardar(Mascota entity)
+        {
+            try
+            {
+                if (entity == null)
+                {
+                    throw new NullReferenceException("Error... la raza no puede ser nula");
+                }
+
+                if (entity.Especie == null)
+                {
+                    throw new NullReferenceException("Error... la especie de la raza no puede ser nula");
+                }
+
+                return repoMascota.Guardar(entity);
+            }
+            catch (Exception ex)
+            {
+                return $"Error al guardar raza: {ex.Message}";
+            }
+        }
+
+        public string Modificar(Mascota entity)
+        {
+            try
+            {
+                if (entity == null)
+                {
+                    throw new NullReferenceException("Error... la raza no puede ser nula");
+                }
+
+                if (entity.Especie == null)
+                {
+                    throw new NullReferenceException("Error... la especie de la raza no puede ser nula");
+                }
+
+                return repoMascota.Modificar(entity);
+            }
+            catch (Exception ex)
+            {
+                return $"Error al modificar raza: {ex.Message}";
+            }
+        }
+
+        public string Eliminar(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    throw new ArgumentException("Error... el id debe ser mayor a cero");
+                }
+
+                return repoMascota.Eliminar(id);
+            }
+            catch (Exception ex)
+            {
+                return $"Error al eliminar raza: {ex.Message}";
+            }
+        }
+
+        public List<Mascota> BuscarPorId(int id)
+        {
+            return repoMascota.ConsultarPorEspecie(id);
+        }
+
+        public Mascota BuscarId(int id)
+        {
+            var mascotaBuscada = Consultar().FirstOrDefault<Mascota>(x => x.Id == id);
+            return mascotaBuscada;
+        }
+    }
+}
---
+++ Prueba/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITY;
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DAL_SQLSERVER.BaseDatos baseDatos = new DAL_SQLSERVER.BaseDatos();
+            //Console.WriteLine(baseDatos.AbrirConexion());
+
+            //Especie especie = new Especie(11, "HISTORICOS");
+
+            //DAL_SQLSERVER.EspecieRepository repo = new DAL_SQLSERVER.EspecieRepository();
+
+            ////Console.WriteLine(repo.Guardar(especie));
+
+            //Console.WriteLine("************************************************");
+            //var lista = repo.Consultar();
+
+            //Console.WriteLine("ID       ---     NOMBRE");
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine($"{item.Id} --- {item.Nombre}");
+            //}
+
+            //repo.Eliminar(especie.Id);
+
+            //Console.WriteLine("************************************************");
+            //lista = repo.Consultar();
+
+            //Console.WriteLine("ID       ---     NOMBRE");
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine($"{item.Id} --- {item.Nombre}");
+            //}
+
+
+            //DAL_SQLSERVER.BaseDatos baseDatos = new DAL_SQLSERVER.BaseDatos();
+            //Console.WriteLine(baseDatos.AbrirConexion());
+
+
+            //DAL_SQLSERVER.RazaRepository repo = new DAL_SQLSERVER.RazaRepository();
+            //var lista = repo.Consultar();
+            //Console.WriteLine("ID    ---   NOMBRE  --- ESPECIE");
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine($"{item.Id} --- {item.Nombre}  --- {item.Especie.Nombre}");
+            //}
+            DAL_POSTGRES.EspecieRepository repo = new DAL_POSTGRES.EspecieRepository();
+
+            
+            Console.WriteLine(repo.Guardar(new Especie(1, "aves")));
+            Console.ReadKey();
+        }
+    }
+}
